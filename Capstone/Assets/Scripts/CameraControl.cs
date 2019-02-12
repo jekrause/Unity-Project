939dbd6c3d1e60baee47e6dfe1a89df2f481b160
Mark <markcortez94@gmail.com>
2019-02-11 23:56:09 -0600

    Added split-screen support
    Added split screen support for four players.  To change the number of players, click on the "SceneControl" object and set the HowManyPlayers variable (1-4)

---
+++ Capstone/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+    private int playerNum;
+    private Camera thisCam;
+    private int numOfPlayers;
+
+    // Use this for initialization
+    void Start () {
+
+        thisCam = gameObject.GetComponent<Camera>();
+
+        //for testing
+        numOfPlayers = GameObject.FindWithTag("SceneControl").GetComponent<NumOfPlayers>().HowManyPlayers;
+
+
+        switch (numOfPlayers)
+        {
+            case 1:
+                if (player.name == "Player1")
+                {
+                    thisCam.rect = new Rect(0, 0, 1, 1);
+                    GameObject.FindWithTag("Camera2").active = false;
+                    GameObject.FindWithTag("Camera3").active = false;
+                    GameObject.FindWithTag("Camera4").active = false;
+                }
+                break;
+            case 2:
+                if (player.name == "Player1")
+                {
+                    thisCam.rect = new Rect(0, 0.5f, 1, 0.5f);
+                    GameObject.FindWithTag("Camera3").active = false;
+                    GameObject.FindWithTag("Camera4").active = false;
+                }
+                else if (player.name == "Player2")
+                {
+                    thisCam.rect = new Rect(0, 0, 1, 0.5f);
+                }
+                break;
+            case 3:
+                if (player.name == "Player1")
+                {
+                    thisCam.rect = new Rect(0, 0.5f, 0.5f, 0.5f);
+                    GameObject.FindWithTag("Camera4").active = false;
+                }
+                else if (player.name == "Player2")
+                {
+                    thisCam.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+                }
+                else if (player.name == "Player3")
+                {
+                    thisCam.rect = new Rect(0, 0, 0.5f, 0.5f);
+                }
+                break;
+            case 4:
+                if (player.name == "Player1")
+                {
+                    thisCam.rect = new Rect(0, 0.5f, 0.5f, 0.5f);
+                }
+                else if (player.name == "Player2")
+                {
+                    thisCam.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+                }
+                else if (player.name == "Player3")
+                {
+                    thisCam.rect = new Rect(0, 0, 0.5f, 0.5f);
+                }
+                else if (player.name == "Player4")
+                {
+                    thisCam.rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+                }
+                break;
+        }
+
+        //old
+        //offset = transform.position - player.transform.position;
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        offset = transform.position - player.transform.position;
+
+        //transform.position = player.transform.position;
+
+    }
+
+    // LateUpdate is called once per frame, but after Update
+    void LateUpdate () {
+    
+        transform.position = player.transform.position + offset;
+
+
+    }
+}
---
+++ Capstone/Assets/Scripts/NumOfPlayers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumOfPlayers : MonoBehaviour
+{
+
+    public int HowManyPlayers;
+
+
+    private void Start()
+    {
+        //automatically gets set to "1" if irregular number
+        if (HowManyPlayers < 1 || HowManyPlayers > 4)
+        {
+            HowManyPlayers = 1;
+        }
+    }
+
+}
