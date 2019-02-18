7275bc4e91da827bbff64a70d45351dd6fd4bb29
Isaac <kadera13@gmail.com>
2019-02-18 11:54:02 -0600

    MainMenu, SettingsMenu, GameOver scene
    Main Menu, Settings Menu with listeners to change between the two. A simple GameOver scene as well.

---
+++ Capstone/Assets/SettingsMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    int buttonNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int.TryParse(GetComponentInChildren<Text>().ToString(), out buttonNum);
+        Settings.NumOfPlayers = buttonNum;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Settings.NumOfPlayers == buttonNum)
+        {
+            GetComponent<Button>().image.color = Color.blue;
+        }
+        else
+        {
+            GetComponent<Button>().image.color = Color.red;
+        }
+    }
+}
