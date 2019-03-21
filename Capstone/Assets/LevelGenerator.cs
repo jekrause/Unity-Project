1bb799fa154fb877e3ee9b41b5592a44a95e081c
Isaac <kadera13@gmail.com>
2019-03-21 15:37:00 -0500

    Start of a LevelGenerator

---
+++ Capstone/Assets/LevelGenerator.cs
@@ -0,0 +1,49 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+
+    public ColorToPrefab[] colorMappings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateLevel();
+    }
+
+    void GenerateLevel()
+    {
+        for(int x = 0; x < map.width; ++x) // may need to be x++
+        {
+            for(int y = 0; y < map.height; ++y)
+            {
+                GenerateTile(x, y);
+            }
+        }
+    }
+
+    void GenerateTile(int x, int y)
+    {
+        Color pixelColor = map.GetPixel(x, y);
+
+        if(pixelColor.a == 0) // The pixel is transparent.
+        {
+            return;
+        }
+
+        Debug.Log(pixelColor);
+
+        foreach(ColorToPrefab colorMapping in colorMappings)
+        {
+            if(colorMapping.color.Equals(pixelColor))
+            {
+                Vector2 position = new Vector2(x, y);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+
+}
