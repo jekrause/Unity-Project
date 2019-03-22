ddd788d3a028d83345353214ffa1137e968f533b
Isaac <kadera13@gmail.com>
2019-03-22 13:29:56 -0500

    Level Generator includes Obstacle Generator

--- Capstone/Assets/LevelGenerator.cs
+++ Capstone/Assets/LevelGenerator.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public Texture2D map;
+    public Texture2D terrainMap;
 
-    public ColorToPrefab[] colorMappings;
+    public Texture2D obstacleMap;
+
+    public ColorToPrefab[] colorTerrainMappings;
+
+    public ColorToPrefab[] colorObstacleMappings;
 
     // Start is called before the first frame update
     void Start()
@@ -16,32 +20,64 @@ public class LevelGenerator : MonoBehaviour
 
     void GenerateLevel()
     {
-        for(int x = 0; x < map.width; ++x) // may need to be x++
+        for(int x = 0; x < terrainMap.width; x++) // may need to be x++
         {
-            for(int y = 0; y < map.height; ++y)
+            for(int y = 0; y < terrainMap.height; y++)
             {
-                GenerateTile(x, y);
+                GenerateTerrainTile(x, y);
+            }
+        }
+
+        for (int x = 0; x < obstacleMap.width; x++) // may need to be x++
+        {
+            for (int y = 0; y < obstacleMap.height; y++)
+            {
+                GenerateObstacle(x, y);
             }
         }
     }
 
-    void GenerateTile(int x, int y)
+    void GenerateTerrainTile(int x, int y)
     {
-        Color pixelColor = map.GetPixel(x, y);
+        Color pixelColor = terrainMap.GetPixel(x, y);
 
         if(pixelColor.a == 0) // The pixel is transparent.
         {
             return;
         }
 
+        //Debug.Log(pixelColor);
+
+        foreach(ColorToPrefab colorTerrainMapping in colorTerrainMappings)
+        {
+            if(colorTerrainMapping.color.Equals(pixelColor))
+            {
+                Vector2 position = new Vector2(x*10f, y*10f);
+                Instantiate(colorTerrainMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+
+    void GenerateObstacle(int x, int y)
+    {
+        float fXObstacleTerrainRatio = obstacleMap.width / terrainMap.width;
+        float fYObstacleTerrainRatio = obstacleMap.height / terrainMap.height;
+
+        Color pixelColor = obstacleMap.GetPixel(x, y);
+
+        if (pixelColor.a == 0) // The pixel is transparent.
+        {
+            return;
+        }
+
         Debug.Log(pixelColor);
 
-        foreach(ColorToPrefab colorMapping in colorMappings)
+        foreach (ColorToPrefab colorObstacleMapping in colorObstacleMappings)
         {
-            if(colorMapping.color.Equals(pixelColor))
+            if (colorObstacleMapping.color.Equals(pixelColor))
             {
-                Vector2 position = new Vector2(x*5.12f, y*5.12f);
-                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+                Vector2 position = new Vector2(x * 10f / fXObstacleTerrainRatio , y * 10f / fYObstacleTerrainRatio);
+                Instantiate(colorObstacleMapping.prefab, position, Quaternion.identity, transform);
             }
         }
     }
