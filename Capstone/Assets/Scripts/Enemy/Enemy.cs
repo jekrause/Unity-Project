ad05b4bd364c3c200de1d35c62fa997f67e7f4c1
Isaac <kadera13@gmail.com>
2019-04-01 12:23:18 -0500

    Enemy FillSpots Change

--- Capstone/Assets/Scripts/Enemy/Enemy.cs
+++ Capstone/Assets/Scripts/Enemy/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using System.DateTime;
 
 public class Enemy : MonoBehaviour
 {
@@ -11,8 +12,11 @@ public class Enemy : MonoBehaviour
      * Safe   - Attack, but try to keep your distance.
      */
 
-    public enum MovementTypeEnum {Patrol, Chase, Safe};
-    public enum RayCastDir {Left90 = 0, Left45 = 1, Left20 = 1, Forward = 2, Right20 = 3, Right45 = 4, Right90 = 5 } //raycast angles from player's transform position
+
+    public enum MovementTypeEnum { Patrol, Chase, Safe };
+    public enum RayCastDir { Left90 = 0, Left45 = 1, Left20 = 1, Forward = 2, Right20 = 3, Right45 = 4, Right90 = 5 } //raycast angles from player's transform position
+
+    public enum LookDir { Towards, Away };
 
     // Start is called before the first frame update
     public const int iBaseAttackRate = 1;
@@ -56,13 +60,21 @@ public class Enemy : MonoBehaviour
 
     protected Collider2D[] players; //  possible players in range of minDist
 
-    protected float minDistance = 4f; // closest a enemy will be in Safe Movement
+    protected float minDistance = 10f; // closest a enemy will be in Safe Movement
     bool withInMinDistance = true; // poor bug evasion, but works
+
+    ContactFilter2D layersToAvoid;
+
+    int playersToAvoid;// = LayerMask.GetMask("Player"); // Used by Safe aiMvmt.
 
     void Start()
     {
+        //layersToAvoid.layerMask = 4608;//
+        layersToAvoid.layerMask = LayerMask.GetMask("Obstacles", "Player"); // It may be helpful to put Enemies in seperate layer or get rid of Player layer.
+        playersToAvoid = LayerMask.GetMask("Player");
         fWaitTime = fStartWaitTime;
-        iRandomSpot = Random.Range(0, moveSpots.Length);
+        //iRandomSpot = Random.Range(0, moveSpots.Length);
+        iRandomSpot = 0;
         FillMoveSpots();
         aiMvmt = MovementTypeEnum.Patrol;
         rb = GetComponent<Rigidbody2D>();
@@ -75,7 +87,7 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         //death 
-        if(fHP <= 0)
+        if (fHP <= 0)
         {
             Object.Destroy(gameObject);
             Destroy(this);
@@ -108,8 +120,8 @@ public class Enemy : MonoBehaviour
         {
             aiMvmt = MovementTypeEnum.Patrol;
         }
-        
-        if(((aiMvmt == MovementTypeEnum.Chase && playerTarget != null) ||
+
+        if (((aiMvmt == MovementTypeEnum.Chase && playerTarget != null) ||
            (aiMvmt == MovementTypeEnum.Safe && !withInMinDistance)) && Time.time > fAttackTime)
         {
             //face player
@@ -124,18 +136,21 @@ public class Enemy : MonoBehaviour
             x.GetComponent<Rigidbody2D>().AddForce(x.transform.right * 500);
 
             fAttackTime = Time.time + 1 / iBaseAttackRate;
-        }        
+        }
     }
-    
+
 
     protected void MvmtPatrol()
     {
         //check if we've reached our destination
-        if (Vector2.Distance(transform.position, moveSpots[iRandomSpot]) < 0.2f)
+        // if (Vector2.Distance(transform.position, moveSpots[iRandomSpot]) < 0.2f)// why not 0f
+        if (Vector2.Distance(moveSpots[iRandomSpot], transform.position) <= fMoveSpeed * Time.deltaTime / 2)
+        //if (Vector2.Distance(moveSpots[iRandomSpot], transform.position) <= 0)
         {
-            if(fWaitTime <= 0)
+            if (fWaitTime <= 0)
             {
-                iRandomSpot = Random.Range(0, moveSpots.Length);
+                //iRandomSpot = Random.Range(0, moveSpots.Length); // this will not work unless different FillMoveSpots is implemented
+                iRandomSpot = (++iRandomSpot) % moveSpots.Length;
                 fWaitTime = fStartWaitTime;
                 Debug.Log("Patrol new spot is " + iRandomSpot);
             }
@@ -147,22 +162,22 @@ public class Enemy : MonoBehaviour
         else
         {
             //handle blocked path
-           if (blockedPaths[(int)RayCastDir.Forward] && Vector2.Distance(raycasts[(int)RayCastDir.Forward].transform.position, transform.position) < 5)
+            if (blockedPaths[(int)RayCastDir.Forward] && Vector2.Distance(raycasts[(int)RayCastDir.Forward].transform.position, transform.position) < 5)
             {
                 //listed in order of move priority
                 if (!blockedPaths[(int)RayCastDir.Right20] && !blockedPaths[(int)RayCastDir.Right45])
                 {
                     transform.rotation = Quaternion.AngleAxis(20, Vector3.forward);
                 }
-                else if(!blockedPaths[(int)RayCastDir.Left20] && !blockedPaths[(int)RayCastDir.Left45])
+                else if (!blockedPaths[(int)RayCastDir.Left20] && !blockedPaths[(int)RayCastDir.Left45])
                 {
                     transform.rotation = Quaternion.AngleAxis(-20, Vector3.forward);
                 }
-                else if(!blockedPaths[(int)RayCastDir.Right20])
+                else if (!blockedPaths[(int)RayCastDir.Right20])
                 {
                     transform.rotation = Quaternion.AngleAxis(20, Vector3.forward);
                 }
-                else if(!blockedPaths[(int)RayCastDir.Left20])
+                else if (!blockedPaths[(int)RayCastDir.Left20])
                 {
                     transform.rotation = Quaternion.AngleAxis(-20, Vector3.forward);
                 }
@@ -170,7 +185,7 @@ public class Enemy : MonoBehaviour
                 {
                     transform.rotation = Quaternion.AngleAxis(45, Vector3.forward);
                 }
-                else if(!blockedPaths[(int)RayCastDir.Left45])
+                else if (!blockedPaths[(int)RayCastDir.Left45])
                 {
                     transform.rotation = Quaternion.AngleAxis(-45, Vector3.forward);
                 }
@@ -206,6 +221,7 @@ public class Enemy : MonoBehaviour
         if (Vector2.Distance(transform.position, playerTarget.transform.position) < minDistance)
         {
             withInMinDistance = true;
+            LookAt(LookDir.Away);
             transform.position = Vector2.MoveTowards(transform.position, playerTarget.transform.position, fMoveSpeed * Time.deltaTime * -1f);
             transform.LookAt(2 * transform.position - playerTarget.transform.position);
             //transform.LookAt(playerTarget.transform.position);
@@ -213,25 +229,80 @@ public class Enemy : MonoBehaviour
             //Debug.Log("Player is within minDistance.");
         }
 
-        else 
+        else
         {
             //Debug.Log("Players outside minDistance.");
-            transform.LookAt(playerTarget.transform.position);
+            LookAt(LookDir.Towards);
             withInMinDistance = false;
         }
 
         Debug.Log(playerTarget.transform.position);
     }
 
+    private void LookAt(LookDir lookDir)
+    {
+        switch (lookDir)
+        {
+            case LookDir.Towards:
+                transform.forward = playerTarget.transform.position - transform.position;
+                break;
+            case LookDir.Away:
+                transform.forward = transform.position - playerTarget.transform.position;
+                break;
+        }
+    }
 
+    // Sorry, this took a while and got pretty messy. Many of the extra statements were used for debugging.
     private void FillMoveSpots()
     {
         moveSpots[0] = transform.position;
-        for(int i = 1; i<moveSpots.Length; i++)
+        //fStartWaitTime = 0;
+        //fSpeed = 9f;
+
+        //Random.seed = System.DateTime.Now.Millisecond;
+        float radius = this.GetComponent<CircleCollider2D>().radius;
+        //Debug.Log("radius: " + radius);
+
+        for (int i = 1; i < moveSpots.Length; i++) // last spot must be visible by first and penultimate spots UPDATE: already done in this implementation
         {
             //TODO: check for collision at move spot before adding to list
-            moveSpots[i] = new Vector2(Random.Range(5, 25), Random.Range(5, 25));
+            /*
+            //here the new spot must be obstacle-free from the preceding spot and spot[0]
+            do
+            {
+                //moveSpots[i] = new Vector2(Random.Range(5, 25), Random.Range(5, 25));
+                moveSpots[i] = new Vector2(Random.Range(-5, 6) + moveSpots[i-1].x, Random.Range(-5, 6) + moveSpots[i-1].y);
+
+            } while (Physics2D.Linecast(moveSpots[i - 1], moveSpots[i], layersToAvoid)
+                        || Physics2D.Linecast(moveSpots[i - 1], moveSpots[0], layersToAvoid));
+            */
+            //Collider2D shapeCollider = this.GetComponent<Collider2D>(); // maybe put outside for loop
+            //int j = 0;
+            int p, q; // Not needed if assignment statements are put into the while condition.
+            do
+            {
+                //Debug.Log("Iterations: " + ++j);
+                moveSpots[i] = new Vector2(Random.Range(-10, 10) + moveSpots[i - 1].x, Random.Range(-10, 10) + moveSpots[i - 1].y);
+
+                p = Physics2D.CircleCast(moveSpots[i - 1], radius, moveSpots[i] - moveSpots[i - 1], layersToAvoid, raycasts, Vector2.Distance(moveSpots[i], moveSpots[i - 1]));
+                q = Physics2D.CircleCast(moveSpots[i], radius, moveSpots[0] - moveSpots[i], layersToAvoid, raycasts, Vector2.Distance(moveSpots[0], moveSpots[i]));
+                //Debug.Log("p: " + p + " & q: " + q);
+                /*foreach(RaycastHit2D raycast in raycasts)
+                {
+
+                    Debug.Log("Tag: " + raycast.collider.gameObject.tag);
+                }*/
+                // WARNING: this makes use of raycasts before it is used by other methods
+            } /*while (Physics2D.CircleCast(moveSpots[i - 1], radius, moveSpots[i] - moveSpots[i - 1], layersToAvoid, raycasts, Vector2.Distance(moveSpots[i], moveSpots[i - 1])) > 0
+                    || Physics2D.CircleCast(moveSpots[i], radius, moveSpots[0] - moveSpots[i], layersToAvoid, raycasts, Vector2.Distance(moveSpots[0], moveSpots[i])) > 0);
+            */
+            while (p > 0 || q > 1); // q always hits the original enemy.
+            //while (Physics2D.CircleCast(moveSpots[i - 1], radius, moveSpots[i] - moveSpots[i - 1], LayerMask.GetMask("Obstacles", , raycasts, Vector2.Distance(moveSpots[i], moveSpots[i - 1])) > 0
+            //        && Physics2D.CircleCast(moveSpots[i], radius, moveSpots[0] - moveSpots[i], layersToAvoid, raycasts, Vector2.Distance(moveSpots[0], moveSpots[i])) > 0);
+
+
         }
+
     }
 
     protected void Damaged(float f)
@@ -264,7 +335,7 @@ public class Enemy : MonoBehaviour
         if (playerTarget != null)
         {
             var angle = Mathf.Atan2(playerTarget.transform.position.y, playerTarget.transform.position.x) * Mathf.Rad2Deg;
-           if( Physics2D.Raycast(transform.position, Quaternion.AngleAxis(angle, transform.forward) * transform.right, fVisionDistance).collider == null )
+            if (Physics2D.Raycast(transform.position, Quaternion.AngleAxis(angle, transform.forward) * transform.right, fVisionDistance).collider == null)
             {
                 playerTarget = null;
             }
@@ -277,9 +348,7 @@ public class Enemy : MonoBehaviour
         raycasts[(int)RayCastDir.Left90] = Physics2D.Raycast(transform.position, Quaternion.AngleAxis(90, transform.forward) * transform.right, fVisionDistance / 2);
         raycasts[(int)RayCastDir.Right20] = Physics2D.Raycast(transform.position, Quaternion.AngleAxis(-20, transform.forward) * transform.right, fVisionDistance);
         raycasts[(int)RayCastDir.Right45] = Physics2D.Raycast(transform.position, Quaternion.AngleAxis(-45, transform.forward) * transform.right, fVisionDistance);
-        raycasts[(int)RayCastDir.Right90] = Physics2D.Raycast(transform.position, Quaternion.AngleAxis(-90, transform.forward) * transform.right, fVisionDistance/2);
-
-
+        raycasts[(int)RayCastDir.Right90] = Physics2D.Raycast(transform.position, Quaternion.AngleAxis(-90, transform.forward) * transform.right, fVisionDistance / 2);
 
         foreach (RayCastDir castDir in (RayCastDir[])System.Enum.GetValues(typeof(RayCastDir)))
         {
@@ -293,8 +362,8 @@ public class Enemy : MonoBehaviour
                  * the new enemy is closer than the old enemy THEN
                  * set the target to the enemy detected at index ii
                  */
-                if(playerTarget == null || 
-                    (Vector2.Distance(transform.position, raycasts[ii].collider.gameObject.transform.position) < 
+                if (playerTarget == null ||
+                    (Vector2.Distance(transform.position, raycasts[ii].collider.gameObject.transform.position) <
                     Vector2.Distance(transform.position, playerTarget.transform.position)))
                 {
                     playerTarget = raycasts[ii].collider.gameObject;
@@ -313,15 +382,15 @@ public class Enemy : MonoBehaviour
 
     private int SafteyRadius()
     {
-        players = Physics2D.OverlapCircleAll(transform.position, fVisionDistance);
+        players = Physics2D.OverlapCircleAll(transform.position, fVisionDistance, playersToAvoid);
         int numPlayers = 0;
         if (players.Length > 0)
         {
             playerTarget = new GameObject();
             playerTarget.transform.position = Vector2.zero;
-            foreach(Collider2D player in players)
+            foreach (Collider2D player in players)
             {
-                if(player.gameObject.tag == "Player")
+                if (player.gameObject.tag == "Player")
                 {
                     playerTarget.transform.position += player.transform.position;
                     ++numPlayers;
@@ -330,7 +399,7 @@ public class Enemy : MonoBehaviour
                 }
             }
 
-            //Debug.Log("Players in radius = " + numPlayers);
+            Debug.Log("Players in radius = " + numPlayers);
             if (numPlayers > 0)
             {
                 playerTarget.transform.position /= numPlayers; // Average out the positions of all players
