6f9af5c22800329ae876eaca13cadb90cee51744
Fue <akathyone@gmail.com>
2019-04-05 12:44:10 -0500

    Added loading screen to transition from main menu to sample scene 2

---
+++ Capstone/Assets/Scripts/LoadingScreen/LoadingScreenScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LoadingScreenScript : MonoBehaviour
+{
+    [SerializeField]private Image LoadingBar;
+    [SerializeField] private Text PercentageText;
+    private float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadNextSceneAsync());
+    }
+
+    IEnumerator LoadNextSceneAsync()
+    {
+       // simply using this since it loads the next scene too quick
+        while(timer < 4.5f)
+        {
+            timer += 0.25f;
+            LoadingBar.fillAmount = (timer / 4.5f);
+            PercentageText.text = ((int)((timer / 4.5) * 100)) + "%";
+            yield return new WaitForSeconds(0.25f);
+        }
+        if(timer >= 4.5f)
+        {
+            AsyncOperation level = SceneManager.LoadSceneAsync(1);
+        }
+            
+    }
+}
