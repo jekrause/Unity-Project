b78792b7dcec393fa4c3d1be39a590b0042c6147
Isaac <kadera13@gmail.com>
2019-05-06 12:31:19 -0500

    Changed WarpStar Script

--- Capstone/Assets/WarpStar.cs
+++ Capstone/Assets/WarpStar.cs
@@ -25,16 +25,22 @@ public class WarpStar : MonoBehaviour
             if(other.gameObject.GetComponent<Player>() != null)
             {
                 ((Player)other.gameObject.GetComponent("Player")).transform.position = destination.transform.position;
-                Debug.Log("Entered WarpStar");
+                //Debug.Log("Entered WarpStar");
             }
             else if(other.gameObject.GetComponent<ObstacleBox>() != null)
             {
-                ((ObstacleBox)other.gameObject.GetComponent("Obstacle")).transform.position = destination.transform.position;
+                //Debug.Log("Box detected Warp");
+                ((ObstacleBox)other.gameObject.GetComponent<ObstacleBox>()).transform.position = destination.transform.position;
             }
-            
+            /*else if(other.gameObject.GetComponent<Bullet>() != null)
+            {
+                Debug.Log("Bullet detected Warp");
+                ((Bullet)other.gameObject.GetComponent<Bullet>()).transform.position = destination.transform.position;
 
-                //Vector2 pos = destination.transform.position;
-                //((Player)other.gameObject.GetComponent("Player")).SetLocation(0, 0);
+            }
+            */
+            //Vector2 pos = destination.transform.position;
+            //((Player)other.gameObject.GetComponent("Player")).SetLocation(0, 0);
         }
     }
 
